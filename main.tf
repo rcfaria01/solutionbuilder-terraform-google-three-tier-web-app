4d4ac5748b8df6ad906a83cb9a0d9770f6a2dee1
Roberto Faria <rcfaria@google.com>
2024-03-17 17:08:45 +0000

    CHange Redis memory to 2GB and update the title of the web app

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "google_redis_instance" "main" {
   authorized_network      = google_compute_network.main.name
   connect_mode            = "DIRECT_PEERING"
   location_id             = var.zone
-  memory_size_gb          = 1
+  memory_size_gb          = 2 # Change the memory size to 2GB
   name                    = "${var.deployment_name}-cache"
   display_name            = "${var.deployment_name}-cache"
   project                 = var.project_id
